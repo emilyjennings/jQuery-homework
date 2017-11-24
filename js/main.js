b7a2f80ba128c09f4dca30d21f34819563d6b41a
emilyjennings <emily.jennings.elizabeth@gmail.com>
2017-11-23 19:09:12 -0500

    fixed my homework, Bill

--- js/main.js
+++ js/main.js
@@ -15,20 +15,36 @@ $(document).ready(function(){
           width: 10,
         }), 5000})
 
-  $('h2').click(function(){
+/*  $('h2').click(function(){
     $('h2').animate({
           width: 900,
           position: 'absolute',
           float: 'right'
         }), 4000})
-      });
+      }); */
 
-  $(function(){
+    $('h2').click(function(){
+      $('h2').animate({
+        width: 900,
+        position: 'absolute',
+        float: 'right'
+      }, 4000)
+    })
+
+/*  $(function(){
       $("body").click(function(){
             if ($("h1").is(":visible"))
                 $("p").slideUp(1000);
             else
                 $("h1").slideDown(1000);
-        });
+        }) */
+
+      $("body").click(function(){
+        if ($('h1').is(":visible")){
+            $('p').slideUp(1000);
+         }else{
+          $('h1').slideDown(1000);
+        }
+      })
 
 });
